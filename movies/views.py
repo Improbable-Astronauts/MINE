bcf827fe7b69c686aee8b2e716f09bf1621cc6f0
JasonOnes <jasonr.jones14@gmail.com>
2018-05-19 17:49:07 -0700

    caught None movies-list exception

--- movies/views.py
+++ movies/views.py
@@ -20,48 +20,54 @@ def home(request):
 def movie_list(request):
     ''' diplays list of movies as main body (search on side)'''
 
-    movies = Movie.objects.all().order_by('title')
-    # paginations if to show only five movies per page
-    paginator = Paginator(movies, 5)
-    page = request.GET.get('page')
     try:
-        movies = paginator.page(page)
-    except PageNotAnInteger:
-        movies = paginator.page(1)
-    except EmptyPage:
-        movies = paginator.page(paginator.num_pages)
-    
-    if request.method == "POST":
-       
-        form = SearchForm(request.POST)
-        msg = None
-       
-
-
-        if form.is_valid():
-            movie = form.save(commit=False)
-
-            details = omdb.get(title=movie.title)
-
-            if details:
-                movie.year = details['year']
-                movie.length = details['runtime']
-                try:
-                    movie.save()
-                    messages.add_message(request, messages.SUCCESS, "Movie added")
-                    print("movie saved to db")
-                except:
-                    messages.add_message(request, messages.INFO, "Unable to save")
-                    print("not saved")
-            else:
-                msg = "Movie not found, maybe you can't spell for doo-doo."
         movies = Movie.objects.all().order_by('title')
-        return render(request, 'movies/list.html', {'movies':movies, 'form':form, 'msg':msg, 'page':page,})
+    
+    #if movies:
+    # paginations if to show only five movies per page
+        paginator = Paginator(movies, 5)
+        page = request.GET.get('page')
+        try:
+            movies = paginator.page(page)
+        except PageNotAnInteger:
+            movies = paginator.page(1)
+        except EmptyPage:
+            movies = paginator.page(paginator.num_pages)
+        
+        if request.method == "POST":
+        
+            form = SearchForm(request.POST)
+            msg = None
+       
 
-    else:
-        form = SearchForm()
+
+            if form.is_valid():
+                movie = form.save(commit=False)
+
+                details = omdb.get(title=movie.title)
+
+                if details:
+                    movie.year = details['year']
+                    movie.length = details['runtime']
+                    try:
+                        movie.save()
+                        messages.add_message(request, messages.SUCCESS, "Movie added")
+                        print("movie saved to db")
+                    except:
+                        messages.add_message(request, messages.INFO, "Unable to save")
+                        print("not saved")
+                else:
+                    msg = "Movie not found, maybe you can't spell for doo-doo."
+            movies = Movie.objects.all().order_by('title')
+            return render(request, 'movies/list.html', {'movies':movies, 'form':form, 'msg':msg, 'page':page,})
+
+        else:
+            form = SearchForm()
         
         return render(request, 'movies/list.html', {'movies':movies, 'form':form, 'page':page,})
+    except None:
+        form = SearchForm()
+        return render(request, 'movies/list.html', {'form':form})
 
 def movie_detail(request, title, year):
     # if someone just types in an url that doesn't work return404 not break
